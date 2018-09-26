c3d972963fc87af0020b2b13ed9db6426f95580c
John <john@John-Carroll.home>
2018-09-25 21:30:11 -0400

    building DB Connection

--- index.php
+++ index.php
@@ -2,19 +2,25 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     include "controller/config.php";
-    $new_user = [
-        'username' => 'mark',
-        'password' => 'test_pass_1'
-    ];
-    $new = new User($new_user);
-    echo $new->return_username();
-    //print_r($new);
-    echo "<br /><br /><br />";
-    $q = User::all();
-    foreach($q as $line) print_r($line->attributes());
-    echo "<br /><br /><br />";
-    $new->save();
-    print_r($new);
+
+    $query_params = [
+        'conditions' => [
+            'username LIKE ?',
+            'jcarroll'
+        ]
+    ]
+    $search = User::all([
+        'conditions' => [
+            'username LIKE ?',
+            'jcarroll'
+        ],
+        'order' => 'id asc'
+    ]);
+
+    foreach($search as $db_row){
+        print_r($db_row);
+    }
+
 
 
 ?>
