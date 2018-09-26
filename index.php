db3c58cfbdafad68230992531fb6a451ceca2d45
John <john@John-Carroll.home>
2018-09-25 21:32:24 -0400

    building DB Connection

--- index.php
+++ index.php
@@ -2,18 +2,11 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     include "controller/config.php";
-    
-    $search = User::all([
-        'conditions' => [
-            'username LIKE ?',
-            'jcarroll'
-        ],
-        'order' => 'id asc'
-    ]);
 
-    foreach($search as $db_row){
-        print_r($db_row);
-    }
+    $search = User::find(1);
+
+    echo $search->username . PHP_EOL;
+    echo $search->return_username();
 
 
 
