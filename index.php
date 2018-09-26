98462cb3c39fe68b5a6d694879ed670aab18804f
John <john@John-Carroll.home>
2018-09-25 20:41:17 -0400

    remove config

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    include "controller/config.php";
+    $test = User::all();
+    print_r($test);
+?>
 <html>
     <body>
         With our first 20k in earnings were going back to Montreal for another visit... lets go !!
