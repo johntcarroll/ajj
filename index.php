50ffdfb479366fcd43db85ff81033696e2509a9d
John <john@John-Carroll.home>
2018-09-28 23:47:09 -0400

    login testing

--- index.php
+++ index.php
@@ -4,40 +4,42 @@
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     </head>
     <body>
-        <div class='row'>
-            <div class='col-xs-12'>
-                <h1>Lock Bay Capital</h1>
+        <div class='container'>
+            <div class='row'>
+                <div class='col-xs-12'>
+                    <h1>Lock Bay Capital</h1>
+                </div>
             </div>
-        </div>
-        <div class='row'>
-            <div class='col-xs-4 col-xs-offset-4'>
-                <div class='card'>
-                    <div class='card-header'>
-                        Lock Bay Captial Login
-                    </div>
-                    <div class='card-body'>
-                        <div class='row'>
-                            <div class='col-xs-12'>
-                                <div class='input-group'>
-                                    <div class='input-group-prepend'>
-                                        <i class="fas fa-user"></i>
+            <div class='row'>
+                <div class='col-xs-4 col-xs-offset-4'>
+                    <div class='card'>
+                        <div class='card-header'>
+                            Lock Bay Captial Login
+                        </div>
+                        <div class='card-body'>
+                            <div class='row'>
+                                <div class='col-xs-12'>
+                                    <div class='input-group'>
+                                        <div class='input-group-prepend'>
+                                            <i class="fas fa-user"></i>
+                                        </div>
+                                        <input class='form-control' name='username' placeholder='username' />
                                     </div>
-                                    <input class='form-control' name='username' placeholder='username' />
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='col-xs-12'>
+                                    <div class='input-group'>
+                                        <input class='form-control' name='password' placeholder='password' />
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class='row'>
-                            <div class='col-xs-12'>
-                                <div class='input-group'>
-                                    <input class='form-control' name='password' placeholder='password' />
+                        <div class='card-footer'>
+                            <div class='row'>
+                                <div class='col-xs-12'>
+                                    <button class='btn btn-default'>Login</button>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class='card-footer'>
-                        <div class='row'>
-                            <div class='col-xs-12'>
-                                <button class='btn btn-default'>Login</button>
                             </div>
                         </div>
                     </div>
