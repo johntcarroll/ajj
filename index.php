da0f2f2ae37bb107ce1502436c8947c59120014e
John <john@John-Carroll.home>
2018-09-29 00:07:54 -0400

    login testing

--- index.php
+++ index.php
@@ -3,37 +3,39 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     </head>
     <body>
-        <div class='container'>
-            <div class='row'>
-                <div class='col col-xs-4'>
-                    <div class='card'>
-                        <div class='card-header'>
-                            LBC Login
-                        </div>
-                        <div class='card-body'>
-                            <div class='row'>
-                                <div class='col col-xs-12'>
-                                    <label>Username</label>
-                                    <input class='form-control' name='user' />
+        <form action='controller/login.php' method='post'>
+            <div class='container'>
+                <div class='row'>
+                    <div class='col col-xs-4'>
+                        <div class='card'>
+                            <div class='card-header'>
+                                LBC Login
+                            </div>
+                            <div class='card-body'>
+                                <div class='row'>
+                                    <div class='col col-xs-12'>
+                                        <label>Username</label>
+                                        <input type='text' class='form-control' name='user' />
+                                    </div>
+                                </div>
+                                <div class='row'>
+                                    <div class='col col-xs-12'>
+                                        <label>Password</label>
+                                        <input type='text' class='form-control' name='pass' />
+                                    </div>
                                 </div>
                             </div>
-                            <div class='row'>
-                                <div class='col col-xs-12'>
-                                    <label>Password</label>
-                                    <input class='form-control' name='pass' />
-                                </div>
-                            </div>
-                        </div>
-                        <div class='card-footer'>
-                            <div class='row'>
-                                <div class='col col-xs-12'>
-                                    <button class='btn btn-default'>Login</button>
+                            <div class='card-footer'>
+                                <div class='row'>
+                                    <div class='col col-xs-12'>
+                                        <button type='submit' class='btn btn-default'>Login</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     </body>
 </html>
