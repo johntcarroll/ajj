05147645907f5c488b2e157e6b1587845f96d6a5
John <john@John-Carroll.local>
2018-10-10 19:48:13 -0400

    debugging

--- view/app.php
+++ view/app.php
@@ -2,21 +2,21 @@
     $root = "../";
     include $root . "view/layout/header.php";
 ?>
-<div class='row'>
-    <div class='col col-xs-3'>
+<div class='row custom-padding'>
+    <div class='col col-xs-3 text-center'>
         <button class='btn btn-lg btn-success' id='db-load'>Load from DB</button>
     </div>
-    <div class='col col-xs-3'>
+    <div class='col col-xs-3 text-center'>
         <button class='btn btn-lg btn-danger' id='db-verify'>Verify Sheet</button>
     </div>
-    <div class='col col-xs-3'>
+    <div class='col col-xs-3 text-center'>
         <button class='btn btn-lg btn-warning' id='db-save' disabled>Save to DB</button>
     </div>
-    <div class='col col-xs-3'>
+    <div class='col col-xs-3 text-center'>
         <button class='btn btn-lg btn-danger' id='remove-rows'>Clear Table</button>
     </div>
 </div>
-<div class='row'>
+<div class='row custom-padding'>
     <div class='col col-xs-4'>
         <label>Company</label>
         <select class='form-control filter' name='company_id'>
@@ -32,9 +32,9 @@
         <input class='form-control' type='date' name='date_before'/>
     </div>
 </div>
-<div class='row'>
+<div class='row custom-padding'>
     <div class='col col-xs-12' id='spreadsheet'>
-        
+
     </div>
 </div>
 <?php
