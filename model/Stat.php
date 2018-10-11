aea030769446a9617fae9ff27dfa3929d8087020
John <john@John-Carroll.local>
2018-10-10 20:37:09 -0400

    debugging

--- model/Stat.php
+++ model/Stat.php
@@ -38,7 +38,7 @@ class Stat extends ActiveRecord\Model{
 
         print_r(array_merge(implode(" AND ", $sql_parts), $params));
 
-        $results = Stat::all(['conditions' => array_merge(implode(" AND ", $sql_parts), $params)); // query DB
+        $results = Stat::all(['conditions' => array_merge(implode(" AND ", $sql_parts), $params)]); // query DB
 
         // format rows for hands on table
         $response = [];
